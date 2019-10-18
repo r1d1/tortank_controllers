7df25b973d69c677efeb4b07873252e20405ba97
Erwan <r1d1@github.com>
2019-10-18 21:09:49 +0200

    Fixed package name plus basic callbacks

--- src/basic_controller.py
+++ src/basic_controller.py
@@ -5,21 +5,25 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
 class BasicController():
-	def__init__(self):
-		self.laser_sub = rospy.Subscriber('scan', self.laser_cb, queue_size=1)
-		self.command_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-
-	def laser__cb(self, msg):
-		# store and process data
-		pass
-
-	def timer_cb(self, event):
-		# update speed
-		pass
+    def __init__(self):
+        self.laser_sub = rospy.Subscriber('scan', self.laser_cb, queue_size=1)
+        self.command_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.timer_cb, queue_size=1)
 
 
+    def laser_cb(self, msg):
+        # store and process data
+        print('rvd')
+        pass
+    
+    def timer_cb(self, event):
+        # update speed
+        print('timer')
+        pass
+    
 if __name__ == "__main__":
-	rospy.init_node('tortank')
+    rospy.init_node('tortank')
+    
+    ctrl = BasicController()
+    rospy.spin()
 
-	ctrl = BasicController()
-	rospy.spin()
